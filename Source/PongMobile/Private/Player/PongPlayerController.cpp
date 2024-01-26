8df3a6fccd5028e029f92a2a82484ba25bd0ccc8
Eder Santander <eder@bamtang.com>
2024-01-26 12:12:14 -0500

    Still working on movement

--- Source/PongMobile/Private/Player/PongPlayerController.cpp
+++ Source/PongMobile/Private/Player/PongPlayerController.cpp
@@ -2,4 +2,41 @@
 
 
 #include "Player/PongPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
 
+APongPlayerController::APongPlayerController()
+{
+	bReplicates = true;
+}
+
+void APongPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(PongContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem =ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+
+	Subsystem->AddMappingContext(PongContext, 0);
+	bShowMouseCursor = false;
+	FInputModeGameAndUI InputModeGameAndUI;
+	InputModeGameAndUI.SetLockMouseToViewportBehavior(EMouseLockMode::LockAlways);
+	InputModeGameAndUI.SetHideCursorDuringCapture(true);
+	SetInputMode(InputModeGameAndUI);
+}
+
+void APongPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APongPlayerController::Move);	
+}
+
+void APongPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2d InputAxisVector = InputActionValue.Get<FVector2d>();
+	const FRotator Rotation = GetControlRotation();
+	
+}
--- Source/PongMobile/Public/Player/PongPlayerController.h
+++ Source/PongMobile/Public/Player/PongPlayerController.h
@@ -5,7 +5,9 @@
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "PongPlayerController.generated.h"
-
+struct FInputActionValue;
+class UInputMappingContext;
+class UInputAction;
 /**
  * 
  */
@@ -13,5 +15,19 @@ UCLASS()
 class PONGMOBILE_API APongPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	APongPlayerController();
 	
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+private:
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputMappingContext> PongContext;
+
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	void Move(const struct FInputActionValue& InputActionValue);
 };
