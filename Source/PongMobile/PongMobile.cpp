71ded453b6f778a085d8745a793b802b64b30927
Eder Santander <eder@bamtang.com>
2024-01-26 09:52:52 -0500

    Initial mobile pong commit

---
+++ Source/PongMobile/PongMobile.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PongMobile.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PongMobile, "PongMobile" );
